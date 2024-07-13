931aec679bb92e61b63fd2ddcd5bc9a3b7cb54f2
unknown <raufnx@gmail.com>
2024-07-13 13:20:45 +0600

    Using post method sending data to server is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Create from "./components/Create";
+import Product from "./components/Product";
 function App() {
   return (
-    <div className=" max-w-screen bg-slate-500">
+    <div className=" max-w-screen bg-slate-500 flex flex-col">
       <Create />
+      <Product />
     </div>
   );
 }
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,35 +1,84 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState();
+  const [stock, setStock] = useState();
+  const [price, setPrice] = useState();
+  const [discount, setDiscount] = useState();
+  const [email, setEmail] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://react-interview.1putym.easypanel.host/api/product", {
+        title: title,
+        stock: stock,
+        price: price,
+        discount: discount,
+        email: email,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
-    <div className="w-full bg-slate-500 mx-10 p-3 text-center">
+    <div
+      className="w-full bg-slate-500 mx
+    -10 p-3 text-center"
+    >
       <h1>Create a Product</h1>
       <form
-        action=""
+        onSubmit={handleSubmit}
         method="post"
         className="w-full flex flex-col justify-center items-center gap-5"
       >
         <input
           type="text"
           placeholder="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
           className=" w-1/2 rounded p-2 outline-none"
         />
         <input
           type="number"
           placeholder="stock"
+          value={stock}
+          onChange={(e) => {
+            setStock(e.target.value);
+          }}
           className=" w-1/2 rounded p-2 outline-none"
         />
         <input
           type="number"
           placeholder="price"
+          value={price}
+          onChange={(e) => {
+            setPrice(e.target.value);
+          }}
           className=" w-1/2 rounded p-2 outline-none"
         />
         <input
           type="number"
           placeholder="discount"
+          value={discount}
+          onChange={(e) => {
+            setDiscount(e.target.value);
+          }}
           className=" w-1/2 rounded p-2 outline-none"
         />
         <input
           type="email"
           placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           className=" w-1/2 rounded p-2 outline-none"
         />
         <button
